37cc7e26e8b7d9d39e92f888a186da6e75c2eb82
Kaisar <kaisar.nassipolla@gmail.com>
2024-10-24 22:28:51 +0500

    added new component in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {AsideComponent} from "./components/aside/aside.component";
 import {StuffTableComponent} from "./components/stuff-table/stuff-table.component";
 import {TechniqueTableComponent} from "./components/technique-table/technique-table.component";
+import {MainComponent} from "./components/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsideComponent, StuffTableComponent, TechniqueTableComponent],
+  imports: [RouterOutlet, AsideComponent, StuffTableComponent, TechniqueTableComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
