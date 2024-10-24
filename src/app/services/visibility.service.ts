ff23afd5b832e87247d5a4ff3f2605ee55714b18
Kaisar <kaisar.nassipolla@gmail.com>
2024-10-24 22:55:12 +0500

    update services

--- src/app/services/visibility.service.ts
+++ src/app/services/visibility.service.ts
@@ -8,11 +8,9 @@ export class VisibilityService {
   private techniqueVisibilitySource = new BehaviorSubject<boolean>(false);
   private stuffVisibilitySource = new BehaviorSubject<boolean>(false);
 
-  // Экспозиция Observable для каждой таблицы
   techniqueVisibility$ = this.techniqueVisibilitySource.asObservable();
   stuffVisibility$ = this.stuffVisibilitySource.asObservable();
 
-  // Методы для управления видимостью каждой таблицы
   toggleTechniqueVisibility(isVisible: boolean) {
     this.techniqueVisibilitySource.next(isVisible);
   }
