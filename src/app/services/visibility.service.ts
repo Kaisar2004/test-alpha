05c741cec5c27361986f548b001108fad3500b98
Kaisar <kaisar.nassipolla@gmail.com>
2024-10-26 00:42:54 +0500

    create new service for device

--- src/app/services/visibility.service.ts
+++ src/app/services/visibility.service.ts
@@ -8,10 +8,12 @@ export class VisibilityService {
   private techniqueVisibilitySource = new BehaviorSubject<boolean>(false);
   private stuffVisibilitySource = new BehaviorSubject<boolean>(false);
   private messageVisibilitySource = new BehaviorSubject<boolean>(false);
+  private deviceVisibilitySource = new BehaviorSubject<boolean>(false);
 
   techniqueVisibility$ = this.techniqueVisibilitySource.asObservable();
   stuffVisibility$ = this.stuffVisibilitySource.asObservable();
   messageVisibility$ = this.messageVisibilitySource.asObservable();
+  deviceVisibility$ = this.deviceVisibilitySource.asObservable();
 
   toggleTechniqueVisibility(isVisible: boolean) {
     this.techniqueVisibilitySource.next(isVisible);
@@ -24,15 +26,20 @@ export class VisibilityService {
   toggleMessageVisibility(isVisible: boolean) {
     this.messageVisibilitySource.next(isVisible);
   }
+  toggleDeviceVisibility(isVisible: boolean) {
+    this.deviceVisibilitySource.next(isVisible);
+  }
 
 
   private _closePersonnel = new Subject<void>();
   private _closeTechnique = new Subject<void>();
   private _closeMessage = new Subject<void>();
+  private _closeDevice = new Subject<void>();
 
   public closePersonnel$ = this._closePersonnel.asObservable();
   public closeTechnique$ = this._closeTechnique.asObservable();
   public closeMessage$ = this._closeMessage.asObservable();
+  public closeDevice$ = this._closeDevice.asObservable();
 
   closePersonnel() {
     this._closePersonnel.next();
@@ -44,5 +51,8 @@ export class VisibilityService {
 
   closeMessage() {
     this._closeMessage.next();
+  }
+  closeDevice() {
+    this._closeDevice.next();
   }
 }
