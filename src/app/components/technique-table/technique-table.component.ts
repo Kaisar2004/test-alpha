74cf1673221f4984867509bd5cc28878b3c6503a
Kaisar <kaisar.nassipolla@gmail.com>
2024-10-29 17:15:34 +0500

    delete data and inject data from service

--- src/app/components/technique-table/technique-table.component.ts
+++ src/app/components/technique-table/technique-table.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {VisibilityService} from "../../services/visibility.service";
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-technique-table',
@@ -13,27 +14,11 @@ import {VisibilityService} from "../../services/visibility.service";
   styleUrl: './technique-table.component.css'
 })
 export class TechniqueTableComponent implements OnInit{
-  public info = [
-    {
-      energy: 3,
-      tab: 92051,
-      fullName: "DZ-1800",
-      output: "Сбойка-1",
-      strongest: "А1-23415",
-      timestamp: "20.12.2020 10:45:20",
-    },
-    {
-      energy: 2,
-      tab: 9384,
-      fullName: "2АМ8Д",
-      output: "Вентиляционный штрек",
-      strongest: "А1-23416",
-      timestamp: "20.12.2020 10:47:30",
-    },
-  ];
-
   public isVisible = false;
+
   private visibilityService = inject(VisibilityService);
+  private dataService = inject(DataService);
+  public technique = this.dataService.technique;
 
   ngOnInit() {
     this.visibilityService.techniqueVisibility$.subscribe(isVisible => {
