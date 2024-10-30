9747269a8e108ba2d5a6f58a0071f8f1b69c8d52
Kaisar <kaisar.nassipolla@gmail.com>
2024-10-30 16:26:20 +0500

    create new functions for updating date

--- src/app/components/staff-table/staff-table.component.ts
+++ src/app/components/staff-table/staff-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {VisibilityService} from "../../services/visibility.service";
 import {DataService} from "../../services/data.service";
@@ -13,7 +13,7 @@ import {DataService} from "../../services/data.service";
   templateUrl: './staff-table.component.html',
   styleUrl: './staff-table.component.css'
 })
-export class staffTableComponent implements OnInit{
+export class staffTableComponent implements OnInit, OnDestroy{
   public isVisible = false;
 
   private visibilityService = inject(VisibilityService);
@@ -25,10 +25,32 @@ export class staffTableComponent implements OnInit{
     this.visibilityService.staffVisibility$.subscribe(isVisible => {
       this.isVisible = isVisible;
     });
+
+    this.updateDate();
+    this.intervalId = setInterval(() => this.updateDate(), 5000);
   }
 
   hideStaffTable() {
     this.visibilityService.toggleStaffVisibility(false);
     this.visibilityService.closePersonnel();
+  }
+
+  formattedDate: string = '';
+  private intervalId: any;
+
+  updateDate(): void {
+    const now = new Date();
+    this.formattedDate = `${this.pad(now.getDate())}.${this.pad(now.getMonth() + 1)}.${now.getFullYear()} ` +
+      `${this.pad(now.getHours())}:${this.pad(now.getMinutes())}:${this.pad(now.getSeconds())}`;
+  }
+
+  pad(num: number): string {
+    return num < 10 ? '0' + num : num.toString();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
--- src/app/components/technique-table/technique-table.component.ts
+++ src/app/components/technique-table/technique-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {VisibilityService} from "../../services/visibility.service";
 import {DataService} from "../../services/data.service";
@@ -13,7 +13,7 @@ import {DataService} from "../../services/data.service";
   templateUrl: './technique-table.component.html',
   styleUrl: './technique-table.component.css'
 })
-export class TechniqueTableComponent implements OnInit{
+export class TechniqueTableComponent implements OnInit, OnDestroy{
   public isVisible = false;
 
   private visibilityService = inject(VisibilityService);
@@ -24,10 +24,32 @@ export class TechniqueTableComponent implements OnInit{
     this.visibilityService.techniqueVisibility$.subscribe(isVisible => {
       this.isVisible = isVisible;
     });
+
+    this.updateDate();
+    this.intervalId = setInterval(() => this.updateDate(), 5000);
   }
 
   hideTechniqueTable() {
     this.visibilityService.toggleTechniqueVisibility(false);
     this.visibilityService.closeTechnique();
+  }
+
+  formattedDate: string = '';
+  private intervalId: any;
+
+  updateDate(): void {
+    const now = new Date();
+    this.formattedDate = `${this.pad(now.getDate())}.${this.pad(now.getMonth() + 1)}.${now.getFullYear()} ` +
+      `${this.pad(now.getHours())}:${this.pad(now.getMinutes())}:${this.pad(now.getSeconds())}`;
+  }
+
+  pad(num: number): string {
+    return num < 10 ? '0' + num : num.toString();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
