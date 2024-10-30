0a5edf40ca2396b98a44e02fcec88189ab62415d
Kaisar <kaisar.nassipolla@gmail.com>
2024-10-30 17:17:16 +0500

    create function for updating time

--- src/app/services/dynamic-date.service.ts
+++ src/app/services/dynamic-date.service.ts
@@ -1,26 +1,41 @@
 import {Injectable, OnDestroy} from '@angular/core';
-import {BehaviorSubject, interval, map, Subscription} from "rxjs";
+import {BehaviorSubject, interval, Subscription} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DynamicDateService implements OnDestroy{
+export class DynamicDateService implements OnDestroy {
   private dateSubject = new BehaviorSubject<string>(this.formatDate(new Date()));
+  private timeSubject = new BehaviorSubject<string>(this.formatTime(new Date()));
   private subscription: Subscription;
 
   constructor() {
-    this.subscription = interval(5000)
-      .pipe(map(() => this.formatDate(new Date())))
-      .subscribe(dateString => this.dateSubject.next(dateString));
+
+    this.subscription = interval(5000).subscribe(() => {
+      const now = new Date();
+      this.dateSubject.next(this.formatDate(now));
+      this.timeSubject.next(this.formatTime(now));
+    });
   }
 
   getDateObservable() {
     return this.dateSubject.asObservable();
   }
 
+
+  getTimeObservable() {
+    return this.timeSubject.asObservable();
+  }
+
+
   private formatDate(date: Date): string {
     return `${this.pad(date.getDate())}.${this.pad(date.getMonth() + 1)}.${date.getFullYear()} ` +
       `${this.pad(date.getHours())}:${this.pad(date.getMinutes())}:${this.pad(date.getSeconds())}`;
+  }
+
+
+  private formatTime(date: Date): string {
+    return `${this.pad(date.getHours())}:${this.pad(date.getMinutes())}:${this.pad(date.getSeconds())}`;
   }
 
   private pad(num: number): string {
